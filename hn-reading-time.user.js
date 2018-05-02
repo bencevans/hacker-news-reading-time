07f16a4579a73d44b8fb67b9436dc5ac31f1f3fc
Ben Evans <ben@bluechimp.io>
2018-05-02 19:29:36 +0100

    working

--- hn-reading-time.user.js
+++ hn-reading-time.user.js
@@ -1,3 +1,17 @@
-$('.athing').each((index, el) => {
-    const href = $(el).find('.title a').attr('href');
-});
+const stories = document.getElementsByClassName('storylink')
+const els = document.getElementsByClassName('subtext')
+
+for (let index = 0; index < els.length; index++) {
+    const el = els[index];
+    const url = stories[index].href
+
+    fetch('https://reado.dockstudios.co.uk/api/v1/url?url=' + url)
+        .then(res => res.json())
+        .then(res => {
+            const time = document.createElement('span')
+            // time.innerHTML = ' | 3m22s'
+            time.innerHTML = ' | ' + res.readingTime.text
+            el.appendChild(time)
+        })
+        .catch(console.error)
+}
