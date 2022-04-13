9cfc98e48f38e0c44be792100891160cdd53550f
Ben Evans <Benjamin.Evans@brunel.ac.uk>
2022-04-13 17:35:06 +0100

    reado instance

--- src/hn-reading-time.user.js
+++ src/hn-reading-time.user.js
@@ -1,17 +1,17 @@
-const stories = document.getElementsByClassName('titlelink')
-const els = document.getElementsByClassName('subtext')
+const stories = document.getElementsByClassName("titlelink");
+const els = document.getElementsByClassName("subtext");
 
 for (let index = 0; index < els.length; index++) {
-    const el = els[index];
-    const url = stories[index].href
+  const el = els[index];
+  const url = stories[index].href;
 
-    fetch('https://reado.dockstudios.co.uk/api/v1/url?url=' + url)
-        .then(res => res.json())
-        .then(res => {
-            const time = document.createElement('span')
-            // time.innerHTML = ' | 3m22s'
-            time.innerHTML = ' | ' + res.readingTime.text
-            el.appendChild(time)
-        })
-        .catch(console.error)
+  fetch("https://reado.herokuapp.com/api/v1/url?url=" + url)
+    .then((res) => res.json())
+    .then((res) => {
+      const time = document.createElement("span");
+      // time.innerHTML = ' | 3m22s'
+      time.innerHTML = " | " + res.readingTime.text;
+      el.appendChild(time);
+    })
+    .catch(console.error);
 }
